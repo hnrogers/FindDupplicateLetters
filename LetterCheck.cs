5eabd8bd3a3d7ff16ae85455cc3d0b0ccf85d173
Heather <hnrogers10@gmail.com>
2022-11-17 22:12:39 -0500

    Initial commit

---
+++ LetterCheck.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+namespace DuplicateLetters
+{
+    class Working
+    {
+        public int DuplicateCount(string a)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace DuplicateLetters
+{
+    class Primary
+    {
+        public static void Main()
+        {
+            Working work = new Working();
+
+            Console.WriteLine(work.DuplicateCount("abcde"));
+            Console.WriteLine(work.DuplicateCount("abbcde"));
+            Console.WriteLine(work.DuplicateCount("Indivisibilities"));
+            Console.WriteLine(work.DuplicateCount("Aa"));
+        }
+    }
+
+}
